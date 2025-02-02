6c9d29105477695a9d156739895d63a23ae9f812
aneta <al.szkolenie2025@gmail.com>
2025-02-02 14:10:56 +0100

    dodanie

--- galleries/admin.py
+++ galleries/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Gallery, GalleryImage
+
+
+class GalleryImageInline(admin.TabularInline):
+    model = GalleryImage
+    extra = 1
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    inlines = [GalleryImageInline]
+
+
+@admin.register(GalleryImage)
+class GalleryImageAdmin(admin.ModelAdmin):
+    list_display = ("gallery", "image", "alt")
 
 # Register your models here.

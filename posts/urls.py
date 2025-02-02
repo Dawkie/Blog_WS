d36a7738af562e27bcec1367f41731e8015bdc8a
aneta <al.szkolenie2025@gmail.com>
2025-02-02 13:58:35 +0100

    dodanie widoków

---
+++ posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+app_name = "posts"
+urlpatterns = [
+    path("", views.posts_list, name="list"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("<int:post_id>/", views.post_details, name="details"),
+    path("new/", views.post_create, name="create"),
+
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from django.core.paginator import Paginator
+
 
 # Create your views here.
+
+@login_required
+def posts_list(request):
+    posts = Post.objects.filter(status="published")
+
+    page_number = request.GET.get("page", 1)
+    per_page = request.GET.get("per_page", 50)
+    paginator = Paginator(posts, per_page)
+
+    page_obj = paginator.page(page_number)
+
+    return render(request, "posts/list.html", {"page_obj": page_obj})
+
+
+@login_required
+def post_details(request, post_id):
+    form = None
+    post = get_object_or_404(Post, id=post_id, status="published")
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+
+    if request.user == post.author:
+        form = PostForm(instance=post)
+
+    # post = Post.objects.get(id=post_id, status="published")
+    return render(request, "posts/details.html", {"post": post, "form": form})
+
+
+@login_required
+def post_create(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            if request.user.is_superuser:
+                post.status = "published"
+            post.save()
+            return redirect("posts:list")
+    return render(request, "posts/create.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("posts:list")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "posts/login.html", {"form": form})
